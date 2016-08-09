fed4931e40d724f8667ff65a8ca7ef874da63b0d
vahidhedayati <badvad@gmail.com>
2016-08-09 18:28:44 +0100

    3.0.5 release issues with 3.4-

--- src/main/groovy/remotessh/RemotesshGrailsPlugin.groovy
+++ src/main/groovy/remotessh/RemotesshGrailsPlugin.groovy
@@ -6,14 +6,12 @@ import grails.plugins.*
 class RemotesshGrailsPlugin extends Plugin {
     def grailsVersion = "3.0 > *"
     def title = "Remote SSH Plugin"
-    def author = "Vahid Hedayati"
-    def authorEmail = "badvad@gmail.com"
     def description = 'Uses the Ganymed SSH-2 library to provide RemoteSSH, RemoteSCP, RemoteSCPDir, and RemoteSCPGet'
-    def documentation = "http://grails.org/plugin/remote-ssh"
-    def license = "Apache-2.0"
-    def issueManagement = [system: 'GITHUB', url: 'https://github.com/vahidhedayati/RemoteSSH/issues']
-    def scm = [url: 'https://github.com/vahidhedayati/RemoteSSH']
-
+    def documentation = "https://github.com/vahidhedayati/RemoteSSH"
+    def license = "APACHE"
+    def developers = [name: 'Vahid Hedayati', email: 'badvad@gmail.com']
+    def issueManagement = [system: 'GITHUB', url: 'https://github.com/vahidhedayati/remotessh/issues']
+    def scm = [url: 'https://github.com/vahidhedayati/remotessh']
     Closure doWithSpring() { {->
         sshConfig(SshConfig)
         } 
